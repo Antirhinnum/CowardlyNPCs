b94bfeec2450dabda17c88b6eebca39af58ab20e
Michael Scott <creeper37500@gmail.com>
2022-02-21 19:14:43 -0500

    Release

---
+++ Common/Hooks/HookLoader.cs
@@ -0,0 +1,32 @@
+﻿using MonoMod.Cil;
+using System;
+using Terraria.ModLoader;
+
+namespace CowardlyNPCs.Common.Hooks
+{
+	internal partial class HookLoader : ILoadable
+	{
+		private const string ILFailMessage = "{0} could not patch {1}.";
+		private static Mod _mod;
+
+		public void Load(Mod mod)
+		{
+			_mod = mod;
+
+			IL.Terraria.NPC.AI_007_TownEntities += NPC_AI_007_TownEntities;
+		}
+
+		public void Unload()
+		{
+			_mod = null;
+
+			IL.Terraria.NPC.AI_007_TownEntities -= NPC_AI_007_TownEntities;
+		}
+
+		private static void LogHookFailed(ILContext failingIL)
+		{
+			_mod.Logger.ErrorFormat(ILFailMessage, failingIL.Method.FullName);
+			throw new Exception(string.Format(ILFailMessage, _mod.Name, failingIL.Method.FullName));
+		}
+	}
+}
---
+++ Common/Hooks/IL.Terraria.NPC.AI_007_TownEntities.cs
@@ -0,0 +1,37 @@
+﻿using MonoMod.Cil;
+using Terraria;
+
+namespace CowardlyNPCs.Common.Hooks
+{
+	internal partial class HookLoader
+	{
+		/// <summary>
+		/// Makes NPCs go home if a boss is alive.
+		/// </summary>
+		private void NPC_AI_007_TownEntities(ILContext il)
+		{
+			ILCursor c = new(il);
+
+			#region
+			///	flag: If true, the NPC will go to their home tile. Local ID: 1.\
+			/// Match:
+			///		bool flag = Main.raining;
+			///	Change to:
+			///		bool flag = Main.raining || Main.CurrentFrameFlags.AnyActiveBossNPC;
+
+			if (!c.TryGotoNext(MoveType.Before,
+					i => i.MatchLdsfld<Main>(nameof(Main.raining)),
+					i => i.MatchStloc(1)
+				))
+			{
+				LogHookFailed(il);
+				return;
+			}
+
+			c.Index += 1;
+			c.EmitDelegate((bool flag) => flag || Main.CurrentFrameFlags.AnyActiveBossNPC);
+
+			#endregion
+		}
+	}
+}
